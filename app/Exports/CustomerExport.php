a0b19173fa2be23482f5fb1d92bf58c385ea3737
Nazwan Hilmi <n.hilmi916@gmail.com>
2024-04-09 20:00:32 +0700

    update

---
+++ app/Exports/CustomerExport.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Customer;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class CustomerExport implements FromCollection, WithHeadings, ShouldAutoSize, WithColumnWidths, WithEvents
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $customers =  Customer::select('id', 'name', 'email', 'phone', 'address')->get();
+
+        $customers = $customers->map(function ($customer, $index) {
+            $customer->id = $index + 1;
+            return $customer;
+        });
+
+        return $customers;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Nama',
+            'Email',
+            'Phone',
+            'Address'
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 15,
+            'B' => 25,
+            'C' => 30,
+            'D' => 30,
+            'E' => 25,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $event->sheet->getStyle('A1:E1')->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                    ],
+                    'fill' => [
+                        'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                        'startColor' => [
+                            'argb' => 'FFFF00',
+                        ],
+                    ],
+                    'alignment' => [
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    ],
+                    'borders' => [
+                        'outline' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ]);
+
+                $event->sheet->getStyle('A2:E' . ($event->sheet->getHighestRow()))->applyFromArray([
+                    'alignment' => [
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    ],
+                    'borders' => [
+                        'outline' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ]);
+            },
+        ];
+    }
+}
